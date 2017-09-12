29bef5f969ab7ee3c785cb91609e03f99833733e
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-09-12 11:34:32 +0200

    Inital commit.

---
+++ Text/Show/SanePrecision.hs
@@ -0,0 +1,14 @@
+-- |
+-- Module      : Text.Show.SanePrecision
+-- Copyright   : (c) Justus Sagemüller 2017
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsagemue $ uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+module Text.Show.SanePrecision where
+
+import Prelude hiding (Show)
+import qualified Prelude
