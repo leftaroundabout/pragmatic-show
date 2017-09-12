ebf9b6d815015f7a485dd267813cf9fc2476a3e8
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-09-12 13:35:08 +0200

    Some simple QuickCheck properties.

--- test/tasty/test.hs
+++ test/tasty/test.hs
@@ -13,7 +13,7 @@ module Main where
 import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Test.Tasty.QuickCheck as QC
-import Test.Tasty.QuickCheck ((==>))
+import Test.Tasty.QuickCheck ((==>), testProperty)
 
 import qualified Prelude
 import Prelude hiding (Show(..))
@@ -26,7 +26,16 @@ tests :: TestTree
 tests = testGroup "Tests"
  [ testGroup "Graph structure of webs"
   [ testGroup "Re-Reading of exact types"
-   [
+   [ testProperty "Char" $ readBackEq ([]::[Char])
+   , testProperty "Int" $ readBackEq ([]::[Int])
+   , testProperty "[[Int]]" $ readBackEq ([]::[ [[Int]] ])
+   , testProperty "String" $ readBackEq ([]::[String])
+   , testProperty "String vs standard instance"
+      . QC.expectFailure -- we don't escape non-ASCII Unicode chars, unlike 'Prelude.show'.
+       $ \s -> show s == Prelude.show (s :: String)
    ]
   ]
  ]
+
+readBackEq :: (Show a, Read a, Eq a) => p a -> a -> Bool
+readBackEq _ x = read (show x) == x
