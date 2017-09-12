757c3bcf2afd524ca94ffdb377d74636529e35af
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-09-12 11:49:28 +0200

    The `Show` class.
    So far, basically a copy of the standard one.

--- Text/Show/SanePrecision.hs
+++ Text/Show/SanePrecision.hs
@@ -10,5 +10,18 @@
 
 module Text.Show.SanePrecision where
 
-import Prelude hiding (Show)
+import Prelude hiding (Show(..), shows)
 import qualified Prelude
+
+class Show a where
+  {-# MINIMAL showsPrec | show #-}
+  showsPrec :: Int -> a -> ShowS
+  showsPrec _ x = (show x++)
+  show :: a -> String
+  show = (`shows`"")
+  showList :: [a] -> ShowS
+  showList [] = ("[]"++)
+  showList (x:xs) = ('[':) . shows x . flip (foldr (\y -> (',':) . shows y)) xs . (']':)
+
+shows :: Show a => a -> ShowS
+shows = showsPrec 0
